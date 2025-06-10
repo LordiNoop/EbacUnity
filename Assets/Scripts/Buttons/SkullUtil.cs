1e03083cc714fcfa3c2f4f88a82387d8e31ea1cc
César <cesar.romero.borges@gmail.com>
2025-06-10 17:19:43 -0300

    add script extension and script for Editor

---
+++ Assets/Scripts/Buttons/SkullUtil.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SkullUtil
+{
+    public static void Scale(this Transform t, float size = 1.2f)
+    {
+        t.localScale = Vector3.one * size;
+    }
+
+    public static T GetRandom<T>(this List<T> list)
+    {
+        return list[Random.Range(0, list.Count)];
+    }
+}
